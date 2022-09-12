38b1dff33004633409c1b1c394a2c099ba34cf0b
maclaine <maclainekuria@gmail.com>
2022-09-12 09:39:45 +0300

    updated

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,3 +23,39 @@ class UserController < ApplicationController
             end
           end
         end
+
+        get '/login' do
+            if !logged_in?
+              erb :'user/login'
+            else
+              redirect to '/orders'
+            end
+          end
+    
+          post '/login' do
+            user = User.find_by(username: params[:username])
+            if user && user.authenticate(params[:password])
+              session[:user_id] = user.id
+              redirect to "/orders"
+            else
+              flash[:message] = "Your credentials are not valid!"
+              redirect '/signup'
+            end
+          end
+    
+          get '/logout' do
+            if logged_in?
+              session.destroy
+              redirect to '/login'
+            else
+              redirect to '/'
+            end
+          end
+    
+          private
+    
+            def user_params
+                {:username => params[:username], :password => params[:password]}
+            end
+    
+    end
